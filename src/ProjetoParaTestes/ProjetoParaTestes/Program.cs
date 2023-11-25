131057cfcda548dff882dc7ce4959af122e7071e
Alex Santana <alexstpr@gmail.com>
2023-11-25 10:49:59 -0300

    projeto testes

---
+++ src/ProjetoParaTestes/ProjetoParaTestes/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProjetoParaTestes.Services;
+
+Console.WriteLine("Hello, World!");
+
+CalculoGarantia calculoGarantia = new CalculoGarantia();
+
+Console.WriteLine("Inicio Calculo Parte 1");
+Console.WriteLine($"Margem calculadada parte igual: {calculoGarantia.CalcularMargemParte(-10000000, 4310000, 5000000, 600000, 300000)}");
+Console.WriteLine("\nInicio Calculo Parte 2");
+Console.WriteLine($"Margem calculadada parte igual: {calculoGarantia.CalcularMargemParte(-4000000, 4410000, 5000000, 500000, 300000)}");
+Console.WriteLine("\nInicio Calculo Contra Parte 1");
+Console.WriteLine($"Margem calculadada contraparte igual: {calculoGarantia.CalcularMargemContraParte(6000000, 0, 5000000, 500000, 300000)}");
+Console.WriteLine("\nInicio Calculo Contra Parte 2");
+Console.WriteLine($"Margem calculadada contraparte igual: {calculoGarantia.CalcularMargemContraParte(6000000, -400000, 5000000, 700000, 300000)}");
+Console.WriteLine("\nInicio Calculo Contra Parte 3");
+Console.WriteLine($"Margem calculadada contraparte igual: {calculoGarantia.CalcularMargemContraParte(6000000, -2000000, 5000000, 700000, 300000)}");
+Console.WriteLine("\nInicio Calculo Contra Parte 4");
+Console.WriteLine($"Margem calculadada contraparte igual: {calculoGarantia.CalcularMargemContraParte(6000000, -1400000, 5000000, 700000, 300000)}");
+Console.WriteLine("\nInicio Calculo Contra Parte 5");
+Console.WriteLine($"Margem calculadada contraparte igual: {calculoGarantia.CalcularMargemContraParte(3000000, -3100000, 5000000, 500000, 3000000)}");
---
+++ src/ProjetoParaTestes/ProjetoParaTestes/Services/CalculoGarantia.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoParaTestes.Services
+{
+    public class CalculoGarantia
+    {
+        public decimal CalcularMargemParte(decimal valorExposicao, decimal valorDepositado, decimal valorLimite, decimal valorMinimoParte, decimal valorMinimoContraParte)
+        {
+            var calculoProvisorio = valorExposicao + valorLimite;
+            Console.WriteLine($"Valor provisorio: {calculoProvisorio}");
+            var saldo = valorDepositado + calculoProvisorio;
+            Console.WriteLine($"Valor Saldo: {saldo}");
+
+            var calculoFinal = calculoProvisorio > 0 ? (valorDepositado != 0 ? valorDepositado : 0) : saldo;
+            Console.WriteLine($"Calculo Final: {calculoFinal}");
+
+            var chamadaMargem = calculoFinal > 0 ? 
+                (Math.Abs(calculoFinal) > Math.Abs(valorMinimoContraParte) ? calculoFinal: 0) :
+                (Math.Abs(calculoFinal) > Math.Abs(valorMinimoParte) ? calculoFinal : 0);
+
+            return chamadaMargem;
+        }
+
+        public decimal CalcularMargemContraParte(decimal valorExposicao, decimal valorDepositado, decimal valorLimite, decimal valorMinimoParte, decimal valorMinimoContraParte)
+        {
+            var calculoProvisorio = valorExposicao + (valorLimite < 0 ? valorLimite : valorLimite *-1);
+            Console.WriteLine($"Valor provisorio: {calculoProvisorio}");
+            var saldo = valorDepositado + calculoProvisorio;
+            Console.WriteLine($"Valor Saldo: {saldo}");
+
+            var calculoFinal = calculoProvisorio < 0 ? (valorDepositado != 0 ? valorDepositado : 0) : saldo;
+            Console.WriteLine($"Calculo Final: {calculoFinal}");
+            var chamadaMargem = calculoFinal < 0 ?
+                (Math.Abs(calculoFinal) > Math.Abs(valorMinimoContraParte) ? calculoFinal : 0) :
+                (Math.Abs(calculoFinal) > Math.Abs(valorMinimoParte) ? calculoFinal : 0);
+
+            return chamadaMargem;
+        }
+    }
+}
