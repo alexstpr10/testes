e7dfd412e39bbe9219b689be0d7a9f71e4a30b80
Alex Santana <alexstpr@gmail.com>
2023-11-25 12:52:32 -0300

    Ajuste Testes

--- src/ProjetoParaTestes/ProjetoParaTestes/Program.cs
+++ src/ProjetoParaTestes/ProjetoParaTestes/Program.cs
@@ -9,6 +9,8 @@ Console.WriteLine("Inicio Calculo Parte 1");
 Console.WriteLine($"Margem calculadada parte igual: {calculoGarantia.CalcularMargemParte(-10000000, 4310000, 5000000, 600000, 300000)}");
 Console.WriteLine("\nInicio Calculo Parte 2");
 Console.WriteLine($"Margem calculadada parte igual: {calculoGarantia.CalcularMargemParte(-4000000, 4410000, 5000000, 500000, 300000)}");
+Console.WriteLine("\nInicio Calculo Parte 3");
+Console.WriteLine($"Margem calculadada parte igual: {calculoGarantia.CalcularMargemParte(-5600000, 1000000, 5000000, 500000, 300000)}");
 Console.WriteLine("\nInicio Calculo Contra Parte 1");
 Console.WriteLine($"Margem calculadada contraparte igual: {calculoGarantia.CalcularMargemContraParte(6000000, 0, 5000000, 500000, 300000)}");
 Console.WriteLine("\nInicio Calculo Contra Parte 2");
